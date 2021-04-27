f12ebe56610c228118cb3255e98ac3495e029b2b
Peter Schlamp <p.schlamp@westernhealth.com>
2021-04-26 18:52:42 -0700

    init

---
+++ main.py
@@ -0,0 +1,34 @@
+# pip install stomp.py
+# http://jasonrbriggs.github.io/stomp.py/api.html#establishing-a-connection
+# run: docker run -p 61613:61613 -p 8161:8161 rmohr/activemq
+# broker (admin - password) 61613 is stomp port
+# activemq admin: http://localhost:8161/admin/ (admin - admin)
+# run this script: python main.py This is a test
+
+# this main.py is the publisher of a topic
+# run the 2 receiver pys first for a good demo
+
+
+import time
+import sys
+
+import stomp
+
+class MyListener(stomp.ConnectionListener):
+    def on_error(self, frame):
+        print('received an error "%s"' % frame.body)
+
+    def on_message(self, frame):
+        print('received a message "%s"' % frame.body)
+
+print("listening on main", flush=True)
+conn = stomp.Connection([('localhost', 61613)])
+conn.set_listener('', MyListener())
+conn.connect('admin', 'password', wait=True)
+#conn.subscribe(destination='/queue/test', id=1, ack='auto')
+conn.subscribe(destination='/topic/test', id=1, ack='auto')
+#conn.send(body=' '.join(sys.argv[1:]), destination='/queue/test')
+conn.send(body=' '.join(sys.argv[1:]), destination='/topic/test')
+time.sleep(2)
+conn.disconnect()
+
---
+++ receiver1.py
@@ -0,0 +1,28 @@
+# pip install stomp.py
+# run: docker run -p 61613:61613 -p 8161:8161 rmohr/activemq
+# broker (admin - password) 61613 is stomp port
+# activemq admin: http://localhost:8161/admin/ (admin - admin)
+# run this script: python main.py This is a test
+
+
+import time
+import sys
+
+import stomp
+
+class MyListener1(stomp.ConnectionListener):
+    def on_error(self, frame):
+        print('received an error "%s"' % frame.body, flush=True)
+
+    def on_message(self, frame):
+        print('received a message "%s"' % frame.body, flush=True)
+
+print("listening on receiver1", flush=True)
+conn = stomp.Connection([('localhost', 61613)])
+conn.set_listener('', MyListener1())
+conn.connect('admin', 'password', wait=True)
+conn.subscribe(destination='/topic/test', id=1, ack='auto')
+
+time.sleep(200)
+conn.disconnect()
+
